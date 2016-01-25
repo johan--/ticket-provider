6b4d74dd8050289916bbf9f3b2781bc6b5837341
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-25 22:40:19 +0700

    [#112304721] Add uid to model that have uid

--- db/migrate/20160125072211_create_accounts.rb
+++ db/migrate/20160125072211_create_accounts.rb
@@ -2,6 +2,7 @@ class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
       t.string :name, null: false
+      t.string :uid, null: false
       t.text :description
       t.string :logo
       t.string :cover_photo
--- db/migrate/20160125083905_create_tickets.rb
+++ db/migrate/20160125083905_create_tickets.rb
@@ -3,6 +3,7 @@ class CreateTickets < ActiveRecord::Migration
     create_table :tickets do |t|
       t.references :ticket_type, index: true, foreign_key: true
       t.references :user, index: true
+      t.string :uid, null: false
       t.string :zone
       t.string :row
       t.string :column
