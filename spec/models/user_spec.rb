cf11441826f5a88a2dc76e7ba2d368ddc7d0ca63
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-25 15:37:34 +0700

    [#112304669] Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'db' do
+    context 'columns' do
+      it { should have_db_column(:email).of_type(:string).with_options(null: false) }
+    end
+
+    context 'indexes' do
+      it { should have_db_index(:email).unique(true) }
+    end
+  end
 end
