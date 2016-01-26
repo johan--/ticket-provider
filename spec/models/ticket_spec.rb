405ea1ba089b978d00688a3fb7a5d0a1edeb71f8
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-26 22:01:46 +0700

    [#112305011] Add state section in ticket model spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe Ticket, type: :model do
 
     it { is_expected.to validate_presence_of(:ticket_type) }
   end
+
+  describe 'state' do
+    let(:account) { Fabricate(:account) }
+    let(:event) { Fabricate(:event, account: account) }
+    let(:ticket_type) { Fabricate(:ticket_type, event: event) }
+    let(:ticket) { Fabricate(:ticket, ticket_type: ticket_type) }
+
+    context 'initial state' do
+      subject { ticket.current_state }
+
+      it { is_expected.to eq 'new' }
+    end
+
+    context 'transition to sold without user' do
+      subject { ticket.transition_to(:sold) }
+
+      it { is_expected.to be_falsey }
+    end
+  end
 end
