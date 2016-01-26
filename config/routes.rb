3f745eb970ce81c54c582b00b60383ecdf9e8a89
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-26 17:05:35 +0700

    [#112400715] Add ticket types destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   scope 'api/v1', module: 'api/v1' do
     resources :events, only: [:index, :create, :update, :destroy]
 
-    resources :ticket_types, only: [:create, :update]
+    resources :ticket_types, only: [:create, :update, :destroy]
   end
 end
