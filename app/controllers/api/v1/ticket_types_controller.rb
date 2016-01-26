fa23f6223bdd7001c7e2bdfc8cb0d621c825b9d4
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-26 11:28:21 +0700

    [#112304943] Implement update method in ticket types controller

--- app/controllers/api/v1/ticket_types_controller.rb
+++ app/controllers/api/v1/ticket_types_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::TicketTypesController < Api::V1::ApiController
   before_action :authenticate_organizer!
 
+  load_resource find_by: :uid, except: :create
+  authorize_resource
+
   def create
-    @ticket_type = TicketType.new(ticket_type_params)
+    @ticket_type = TicketType.new(ticket_type_params.merge(event: Event.find_by_uid(params[:ticket_type][:event_id])))
 
     if @ticket_type.save
       render json: @ticket_type, status: :created
+    else
+      render json: { errors: [@ticket_type.errors.full_messages.to_sentence] }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @ticket_type.update_attributes(ticket_type_params)
+      render json: @ticket_type, status: :ok
     else
       render json: { errors: [@ticket_type.errors.full_messages.to_sentence] }, status: :unprocessable_entity
     end
@@ -19,6 +30,5 @@ class Api::V1::TicketTypesController < Api::V1::ApiController
       .permit(:name,
               :description,
               :current_price)
-      .merge(event: Event.find_by_uid(params[:ticket_type][:event_id]))
   end
 end
