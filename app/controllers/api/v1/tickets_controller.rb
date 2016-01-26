1572710c11e33d2803347e8d1c2fbc66a4ef49f6
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-26 22:02:53 +0700

    [#112305011] Implement tickets controller update method

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::TicketsController < Api::V1::ApiController
   before_action :authenticate_organizer!
 
+  load_resource find_by: :uid, except: :create
+  authorize_resource
+
   def create
     @ticket = Ticket.new(ticket_params.merge(ticket_type: TicketType.find_by_uid(params[:ticket][:ticket_type_id])))
 
@@ -8,6 +11,15 @@ class Api::V1::TicketsController < Api::V1::ApiController
       render json: @ticket, status: :created
     else
       render json: { errors: [@ticket.errors.full_messages.to_sentence] }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @ticket.user = User.find_by_uid(params[:ticket][:user_id]) if params[:ticket][:user_id]
+    if @ticket.transition_to(ticket_state_params[:state])
+      render json: @ticket, status: :ok
+    else
+      render json: { errors: [I18n.t('backend.ticket.cannot_transition_to', state: ticket_state_params[:state])] }, status: :unprocessable_entity
     end
   end
 
@@ -20,5 +32,12 @@ class Api::V1::TicketsController < Api::V1::ApiController
               :row,
               :column,
               :price)
+  end
+
+
+  def ticket_state_params
+    params
+      .require(:ticket)
+      .permit(:state)
   end
 end
