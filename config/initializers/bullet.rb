ed13ea5d06f69225d83ea03c4ba98db3f31142f5
Earth <saritsuriya@hotmail.com>
2016-01-26 16:59:37 +0700

    [#112350461] Initialize Bullet

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,9 @@
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.add_footer = true
+  Bullet.rails_logger = true
+  Bullet.raise = true
+end
