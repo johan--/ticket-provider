c8a257165a6e32ecc3b7467b83962f5b1c1ad7ac
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-01-26 23:44:36 +0700

    [#112305159] Add account permission in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,10 +10,12 @@ class Ability
           can :manage, :all
           can :access, :rails_admin
         elsif user.role == 'account_owner'
+          can :manage, Account, id: user.account_id
           can :manage, Event, account: user.account
           can :manage, TicketType, event_id: user.account.event_ids
           can :manage, Ticket, ticket_type: { event_id: user.account.event_ids }
         elsif user.role == 'team_member'
+          can [:read, :create, :update], Account, id: user.account_id
           can [:read, :create, :update], Event, account: user.account
           can [:read, :create, :update], TicketType, event_id: user.account.event_ids
           can :manage, Ticket, ticket_Type: { event_id: user.account.event_ids }
