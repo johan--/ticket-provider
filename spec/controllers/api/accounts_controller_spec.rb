7513be7d398908d04f197636164c925454cdf6f1
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-02-11 11:29:24 +0700

    [#112305199] Implement update method in accounts controller spec

--- spec/controllers/api/accounts_controller_spec.rb
+++ spec/controllers/api/accounts_controller_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
       it { expect(response).to match_response_schema('errors') }
     end
   end
+
+  describe 'PUT #update' do
+    let(:account) { Fabricate(:account) }
+    let(:organizer) { Fabricate(:account_owner, account: account) }
+
+    before { sign_in :organizer, organizer }
+
+    context 'when account params is valid' do
+      before { put :update, id: account.uid, account: { name: 'something' } }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to match_response_schema('account') }
+      it { expect(JSON.parse(response.body)['account']['name']).to eq 'something' }
+    end
+
+    context 'when account params is invalid' do
+      before { put :update, id: account.uid, account: { name: '' } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+      it { expect(response).to match_response_schema('errors') }
+    end
+  end
 end
