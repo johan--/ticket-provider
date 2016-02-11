74b5f6c1eaf69efdc04e9475957e993aa23bd764
Earth <saritsuriya@hotmail.com>
2016-02-11 21:37:49 +0700

    [#113250591] Add seat_type to class

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -5,7 +5,9 @@ class TicketType < ActiveRecord::Base
   validates :name, presence: true
   validates :event, presence: true
 
-  before_create :set_uid
+  before_create :set_uid, :set_default_seat_type
+
+  enum seat_type: [ :fix_seat, :non_fix_seat ]
 
   private
 
@@ -13,5 +15,9 @@ class TicketType < ActiveRecord::Base
     begin
       self.uid = SecureRandom.hex(4)
     end while (self.class.exists?(uid: self.uid))
+  end
+
+  def set_default_seat_type
+    self.seat_type = TicketType.seat_types[:fix_seat] unless self.seat_type
   end
 end
