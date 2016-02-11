0b23bdb32d1b5ea64dc2923b803e6e95b8868428
Earth <saritsuriya@hotmail.com>
2016-02-11 21:05:49 +0700

    [#113250671] Remove zone from fabrication

--- spec/fabricators/ticket_fabricator.rb
+++ spec/fabricators/ticket_fabricator.rb
@@ -1,5 +1,4 @@
 Fabricator(:ticket, class_name: 'Ticket') do
-  zone { (('A'..'Z').to_a << nil).sample }
   row { (('A'..'Z').to_a << nil).sample }
   column { ((1..10).to_a << nil).sample }
   price { (500...10000).to_a.sample }
