2ea25157bf33385980761a0e8ebc6bd7b88972df
Earth <saritsuriya@hotmail.com>
2016-02-16 19:09:49 +0700

    [#113686685] Remove unnecessary test case

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,16 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe 'POST #create' do
-
-    context 'when request is created by user' do
-
-      context 'when user is valid' do
-        before { post :create, user: Fabricate.attributes_for(:user) }
-
-        it { expect(response).to have_http_status(:created) }
-      end
-
-      context 'when email is already use' do
+      context 'when user params is invalid' do
         before do
           Fabricate(:user, email: 'earth@earth.com')
           post :create, user: Fabricate.attributes_for(:user, email: 'earth@earth.com')
@@ -21,6 +12,5 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         it { expect(response).to have_http_status(:unprocessable_entity) }
         it { expect(response).to match_response_schema('errors') }
       end
-    end
   end
 end
