9b133b15989c1623ccaa8b2c62054e1618596bf6
Earth <saritsuriya@hotmail.com>
2016-02-20 17:13:06 +0700

    [#113686695] Import locale to website

--- backend/app.jsx
+++ backend/app.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Backbone from 'backbone';
 import Router from './router.jsx';
-import InterfaceComponent from './components/interface-component.jsx'
+import InterfaceComponent from './components/interface-component.jsx';
+
+I18n.locale = window.locale;
+I18n.fallbacks = true;
 
 // Initialize app router.
 let router = new Router();
