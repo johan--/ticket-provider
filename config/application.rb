f09402111a9e94b12ad23a269f04ca91e87d56eb
Earth <saritsuriya@hotmail.com>
2016-02-20 17:58:35 +0700

    [#113686695] Delete unused code

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module TicketProvider
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :th]
-    config.middleware.use I18n::JS::Middleware
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
