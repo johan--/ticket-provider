c19afdeca43b30350191b6027764154305302941
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-02-23 20:03:19 +0700

    [#112305225] Add authenticate_organizer! method in backend controller

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,11 +1,8 @@
 class BackendController < ApplicationController
   before_action :set_locale
+  before_action :authenticate_organizer!
 
   layout 'backend'
-
-  def index
-
-  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
