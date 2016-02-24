3122e7064646a24e21c3acf36bb369e045220f5d
Earth <saritsuriya@hotmail.com>
2016-02-24 16:23:00 +0700

    [#112308011]  Fix spec error

--- spec/features/backend/events_list_spec.rb
+++ spec/features/backend/events_list_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'Organizer can retrieve event belongs to their account' do
-  let(:account) { Fabricate(:account) }
+feature 'Organizer can retrieve event belongs to their account', js: true do
+  let!(:account) { Fabricate(:account) }
   let(:organizer) { Fabricate(:account_owner, account: account) }
 
   before do
@@ -11,7 +11,7 @@ feature 'Organizer can retrieve event belongs to their account' do
 
   scenario 'Organizer has events in their account', js: true do
     visit '/app/events'
-
+    wait_for_async_request
     expect(all('.event-item').size).to eq Event.count
   end
 end
