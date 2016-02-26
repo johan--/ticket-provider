b3bfa5f96605b6086ac8be4192a8326abff8a9a1
Earth <saritsuriya@hotmail.com>
2016-02-26 13:39:33 +0700

    [#112308011] uncomment spec

--- spec/features/backend/authenticate_spec.rb
+++ spec/features/backend/authenticate_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-# feature 'Organizer can login to their account', js: true do
-#   let(:account) { Fabricate(:account) }
-#   let(:organizer) { Fabricate(:account_owner, account: account) }
-#
-#   scenario 'Organizer fill all field', js: true do
-#     visit new_organizer_session_path
-#     fill_in 'organizer[email]', with: organizer.email
-#     fill_in 'organizer[password]', with: organizer.password
-#     click_button 'Login'
-#
-#     expect(page).to have_content('Event')
-#   end
-# end
+feature 'Organizer can login to their account', js: true do
+  let(:account) { Fabricate(:account) }
+  let(:organizer) { Fabricate(:account_owner, account: account) }
+
+  scenario 'Organizer fill all field', js: true do
+    visit new_organizer_session_path
+    fill_in 'organizer[email]', with: organizer.email
+    fill_in 'organizer[password]', with: organizer.password
+    click_button I18n.t('backend.authentication.login')
+
+    expect(page).to have_content('Event')
+  end
+end
 
 feature 'Organizer can register to their account', js: true do
   let!(:account) { Fabricate.attributes_for(:account) }
@@ -25,7 +25,7 @@ feature 'Organizer can register to their account', js: true do
     fill_in 'organizer[account_name]', with: account[:name]
     fill_in 'organizer[password]', with: organizer[:password]
     fill_in 'organizer[password_confirmation]', with: organizer[:password]
-    click_button 'Register'
+    click_button I18n.t('backend.authentication.register')
 
     expect(page).to have_content('Event')
     expect(Organizer.first.email).to eq organizer[:email]
