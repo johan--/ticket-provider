c1a663925337e4560d7205a010247f9372a253ba
Earth <saritsuriya@hotmail.com>
2016-03-01 16:40:40 +0700

    [#114474637] Add update def to controller

--- app/controllers/api/v1/organizers_controller.rb
+++ app/controllers/api/v1/organizers_controller.rb
@@ -1,7 +1,27 @@
 class Api::V1::OrganizersController < Api::V1::ApiController
   before_action :authenticate_organizer!, only: [:me]
 
+  load_and_authorize_resource find_by: :uid
+
   def me
     render json: current_organizer, serializer: OrganizerSerializer, status: :ok
+  end
+
+  def update
+    @organizer = current_organizer
+    if @organizer.update_attributes(organizer_params)
+      render json: @organizer, status: :ok
+    else
+      render json: { errors: [@organizer.errors.full_messages.to_sentence] }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def organizer_params
+    params
+        .require(:organizer)
+        .permit(:email,
+                :name)
   end
 end
