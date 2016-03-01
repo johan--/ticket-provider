7180ae42461520ca50d1f71eb057bb8802a609be
Earth <saritsuriya@hotmail.com>
2016-03-01 11:37:18 +0700

    [#114474609] Remove unnecessary def

--- app/controllers/api/v1/organizers_controller.rb
+++ app/controllers/api/v1/organizers_controller.rb
@@ -1,25 +1,11 @@
 class Api::V1::OrganizersController < Api::V1::ApiController
-
   before_action :authenticate_organizer!, only: [:me]
-
-  load_and_authorize_resource find_by: :uid
 
   def me
     @organizer = current_organizer
-    p @organizer
+
     if @organizer
       render json: @organizer, status: :ok
-    else
-      render json: { errors: [@organizer.errors.full_messages.to_sentence] }, status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def organizer_params
-    params
-        .require(:organizer)
-        .permit(:email,
-                :name)
   end
 end
