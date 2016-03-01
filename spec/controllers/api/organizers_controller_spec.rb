8a9490b966a04ce1b1a7048465d81383dc274ba9
Earth <saritsuriya@hotmail.com>
2016-03-01 16:40:52 +0700

    [#114474637] Add update spec

--- spec/controllers/api/organizers_controller_spec.rb
+++ spec/controllers/api/organizers_controller_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe Api::V1::OrganizersController, type: :controller do
       it { expect(response).to match_response_schema('organizer') }
     end
   end
+
+  describe 'PUT #update' do
+    context 'when organizer update profile' do
+      let(:account) { Fabricate(:account) }
+      let(:other_account) { Fabricate(:account) }
+      let(:organizer) { Fabricate(:account_owner, account: account) }
+
+      before do
+        sign_in :organizer, organizer
+        put :update,id: organizer.uid, organizer: { name: 'aun' }
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to match_response_schema('organizer') }
+      it { expect(JSON.parse(response.body)['organizer']['name']).to eq 'aun' }
+    end
+  end
 end
