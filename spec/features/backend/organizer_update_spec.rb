24c0d3cc759fe8f1bef4b2cc24bf543a49dbc52b
Earth <saritsuriya@hotmail.com>
2016-03-08 15:30:22 +0700

    [#114474167] Fix spec

--- spec/features/backend/organizer_update_spec.rb
+++ spec/features/backend/organizer_update_spec.rb
@@ -11,10 +11,10 @@ feature 'Organizer can update profile' do
   scenario 'Organizer fill in all required information', js: true do
     visit "/app/organizers"
 
-    fill_in 'name', with: organizer.email
-    fill_in 'Password', with: '1q2w3e4r'
-    fill_in 'Confirm Password', with: '1q2w3e4r'
-    fill_in 'Current Password', with: organizer.password
+    fill_in I18n.t('backend.authentication.name'), with: organizer.name
+    fill_in I18n.t('backend.authentication.new_password'), with: '1q2w3e4r'
+    fill_in I18n.t('backend.authentication.password_confirmation'), with: '1q2w3e4r'
+    fill_in I18n.t('backend.authentication.current_password'), with: organizer.password
     click_button I18n.t('backend.organizers.update')
     wait_for_async_request
 
@@ -24,9 +24,9 @@ feature 'Organizer can update profile' do
   scenario 'Organizer missed some required information', js: true do
     visit "/app/organizers"
 
-    fill_in 'name', with: organizer.email
-    fill_in 'Password', with: '1q2w3e4r'
-    fill_in 'Confirm Password', with: '1q2w3e4r'
+    fill_in I18n.t('backend.authentication.name'), with: organizer.name
+    fill_in I18n.t('backend.authentication.new_password'), with: '1q2w3e4r'
+    fill_in I18n.t('backend.authentication.password_confirmation'), with: '1q2w3e4r'
     click_button I18n.t('backend.organizers.update')
     wait_for_async_request
 
