9f07155d033b61721c1f25db0d52af5ba6b78c05
Earth <saritsuriya@hotmail.com>
2016-03-14 19:09:02 +0700

    [#114474683] Fix spec

--- spec/features/backend/account_setting_spec.rb
+++ spec/features/backend/account_setting_spec.rb
@@ -11,19 +11,22 @@ feature 'Organizer can update Account' do
   scenario 'Organizer fill in all required information', js: true do
     visit "/app/organizers/settings"
 
-    fill_in "account_name", with: "hehe"
-    click_button I18n.t('backend.accounts.save_changes')
-    wait_for_async_request
-
+    within all('.organizer-settings-container form').last do
+      fill_in "account_name", with: "hehe"
+      click_button I18n.t('backend.accounts.save_changes')
+      wait_for_async_request
+    end
     expect(page).to have_content I18n.t('backend.organizers.success_update');
   end
 
   scenario 'Organizer missed name', js: true do
     visit "/app/organizers/settings"
 
-    fill_in "account_name", with: ''
-    click_button I18n.t('backend.accounts.save_changes')
-    wait_for_async_request
+    within all('.organizer-settings-container form').last do
+      fill_in "account_name", with: ''
+      click_button I18n.t('backend.accounts.save_changes')
+      wait_for_async_request
+    end
 
     expect(page).to have_content 'Name can\'t be blank'
   end
--- spec/features/backend/organizer_setting_spec.rb
+++ spec/features/backend/organizer_setting_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 feature 'Organizer can update profile' do
@@ -8,26 +9,45 @@ feature 'Organizer can update profile' do
     sign_in_with(organizer)
   end
 
-  scenario 'Organizer fill in all required information', js: true do
+  scenario 'Organizer change password with valid information', js: true do
     visit "/app/organizers/settings"
 
-    fill_in "organizer_name", with: organizer.name
-    fill_in "password", with: '1q2w3e4r'
-    fill_in "password_confirmation", with: '1q2w3e4r'
-    fill_in "current_password", with: organizer.password
-    click_button I18n.t('backend.organizers.save_changes')
+    within first('.organizer-settings-container form') do
+      fill_in 'password', with: '1q2w3e4r'
+      fill_in 'password_confirmation', with: '1q2w3e4r'
+      fill_in 'current_password', with: organizer.password
+      click_button I18n.t('backend.organizers.save_changes')
+    end
+
     wait_for_async_request
 
-    expect(page).to have_content I18n.t('backend.organizers.success_update');
+    expect(page).to have_content I18n.t('backend.organizers.success_update')
   end
 
-  scenario 'Organizer missed some required information', js: true do
+  scenario 'Organizer change password with valid information', js: true do
     visit "/app/organizers/settings"
 
-    fill_in "organizer_name", with: organizer.name
-    fill_in "password", with: '1q2w3e4r'
-    fill_in "password_confirmation", with: '1q2w3e4r'
-    click_button I18n.t('backend.organizers.save_changes')
+    within first('.organizer-settings-container form') do
+      fill_in 'organizer_name', with: 'some valid name'
+      fill_in 'current_password', with: organizer.password
+      click_button I18n.t('backend.organizers.save_changes')
+    end
+
+    wait_for_async_request
+
+    expect(page).to have_content I18n.t('backend.organizers.success_update')
+  end
+
+  scenario 'Organizer missed required information', js: true do
+    visit "/app/organizers/settings"
+
+    within first('.organizer-settings-container form') do
+      fill_in 'organizer_name', with: organizer.name
+      fill_in 'password', with: '1q2w3e4r'
+      fill_in 'password_confirmation', with: '1q2w3e4r'
+      click_button I18n.t('backend.organizers.save_changes')
+    end
+
     wait_for_async_request
 
     expect(page).to have_content 'Current password can\'t be blank'
