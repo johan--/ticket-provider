f5ab2e558b369b13a928d3a5f1dd0d198d3d9ee5
Swas <zeemyself@gmail.com>
2016-03-16 15:28:04 +0700

    [#114682887] After create event redirect to event page

--- backend/stores/event-store.jsx
+++ backend/stores/event-store.jsx
@@ -89,9 +89,7 @@ class EventCollection extends Store.Collection {
 
         jqXHR.done(() => {
           this.getAll();
-          emitter.emit('success', I18n.t('backend.events.success_create'));
           Backbone.history.navigate('/app/events', true);
-
         });
 
         jqXHR.fail((jqXHR, textStatus, errorThrown) => {
