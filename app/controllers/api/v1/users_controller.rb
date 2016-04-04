6bac747faf50762a826910c0f12dbb3daadb9fa6
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-05 00:07:07 +0700

    [HOT-FIX] Skip load resource on create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < Api::V1::ApiController
   before_action :authenticate_user!, except: :create
 
-  load_and_authorize_resource find_by: :uid
+  load_and_authorize_resource find_by: :uid, except: :create
 
   def me
     if current_user
