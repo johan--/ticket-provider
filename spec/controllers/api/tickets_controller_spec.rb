1825b21e2414bcab64fe326909bab9f80e01faf9
Earth <saritsuriya@hotmail.com>
2016-04-05 20:51:14 +0700

    [#114301411] Fix spec for ticket controller

--- spec/controllers/api/tickets_controller_spec.rb
+++ spec/controllers/api/tickets_controller_spec.rb
@@ -32,14 +32,15 @@ RSpec.describe Api::V1::TicketsController, type: :controller do
     before { sign_in :organizer, organizer }
 
     context 'when ticket params is valid' do
-      before { post :create, ticket: Fabricate.attributes_for(:ticket, ticket_type_id: ticket_type.uid) }
+      before { post :create, ticket: { quantity: 5 }, ticket_type_id: ticket_type.uid }
 
       it { expect(response).to have_http_status(:created) }
-      it { expect(response).to match_response_schema('ticket') }
+      it { expect(response).to match_response_schema('tickets') }
+      it { expect(Ticket.count).to eq 5 }
     end
 
-    context 'when ticket pararms is invalid' do
-      before { post :create, ticket: Fabricate.attributes_for(:ticket) }
+    context 'when ticket params is invalid' do
+      before { post :create, ticket: { quantity: 5 } }
 
       it { expect(response).to have_http_status(:unprocessable_entity) }
       it { expect(response).to match_response_schema('errors') }
