8b495e5a6fcc2d6bb45c19f6e4a49c40219f138f
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-11 16:24:56 +0700

    [#115431609] Fix sign in page error message

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,14 +3,11 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t('errors.messages.not_saved',
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
-   <div class="alert alert-danger alert-block" >
-    #{messages}
-   </div>
+      <div class="alert alert-danger alert-block" >
+        #{messages}
+      </div>
     HTML
 
     html.html_safe
